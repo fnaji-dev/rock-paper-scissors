37224934681c059534d2bebae2c180ce801e78f8
Firas Naji <naji.firas@gmail.com>
2023-02-27 17:34:40 +0900

    Create playRound function
    Assign playerSelection, computerSelection within playRound function
    Check playerSelection against computerSeleciton within playRound func

--- rps-game.js
+++ rps-game.js
@@ -9,32 +9,34 @@ function computerChoice() {
         return "scissors";
     }
 }
+function playRound(playerSelection, computerSelection){
 
-// Assign computer choice to computer selection\
-let computerSelection = computerChoice()
+    // Assign computer choice to computer selection\
+    let computerSelection = computerChoice()
 
-// Ask player for selection input
-// Assign player input to player selection
-let playerSelection = prompt("Enter 'rock', 'paper', or 'scissors'")
-playerSelection = playerSelection.toLowerCase()
+    // Ask player for selection input
+    // Assign player input to player selection
+    let playerSelection = prompt("Enter 'rock', 'paper', or 'scissors'")
+    playerSelection = playerSelection.toLowerCase()
 
-// Check player selection against computer selection
-// Inform player of win/loss
-if (playerSelection === computerSelection) {
-    console.log(`It's a tie, ${playerSelection} against ${computerSelection}!`)
-} else {
-    switch(playerSelection + "-" + computerSelection) {
-        case "rock-scissors":
-        case "paper-rock":
-        case "scissors-paper":
-            console.log(`You win, ${playerSelection} beats ${computerSelection}!`)
-            break;
-        case "rock-paper":
-        case "paper-scissors":
-        case "scissors-rock":
-            console.log(`Oh no, ${playerSelection} loses to ${computerSelection}! `)
-            break;
-        default:
-            console.log(`Oops, ${playerSelection} isn't a valid choice for this game...`)
+    // Check player selection against computer selection
+    // Inform player of win/loss
+    if (playerSelection === computerSelection) {
+        console.log(`It's a tie, ${playerSelection} against ${computerSelection}!`)
+    } else {
+        switch(playerSelection + "-" + computerSelection) {
+            case "rock-scissors":
+            case "paper-rock":
+            case "scissors-paper":
+                console.log(`You win, ${playerSelection} beats ${computerSelection}!`)
+                break;
+            case "rock-paper":
+            case "paper-scissors":
+            case "scissors-rock":
+                console.log(`Oh no, ${playerSelection} loses to ${computerSelection}! `)
+                break;
+            default:
+                console.log(`Oops, ${playerSelection} isn't a valid choice for this game...`)
+        }
     }
 }
