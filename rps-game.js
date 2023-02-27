3038cfe08414d798e9cc7762f4444329ddd65310
Firas Naji <naji.firas@gmail.com>
2023-02-27 17:31:27 +0900

    Add code to check for win/loss
    Check playerSelection against computerSelection
    Show win/loss message in console

--- rps-game.js
+++ rps-game.js
@@ -9,14 +9,32 @@ function computerChoice() {
         return "scissors";
     }
 }
+
 // Assign computer choice to computer selection\
 let computerSelection = computerChoice()
+
 // Ask player for selection input
+// Assign player input to player selection
 let playerSelection = prompt("Enter 'rock', 'paper', or 'scissors'")
 playerSelection = playerSelection.toLowerCase()
 
-// Assign player input to player selection
-
 // Check player selection against computer selection
-
 // Inform player of win/loss
+if (playerSelection === computerSelection) {
+    console.log(`It's a tie, ${playerSelection} against ${computerSelection}!`)
+} else {
+    switch(playerSelection + "-" + computerSelection) {
+        case "rock-scissors":
+        case "paper-rock":
+        case "scissors-paper":
+            console.log(`You win, ${playerSelection} beats ${computerSelection}!`)
+            break;
+        case "rock-paper":
+        case "paper-scissors":
+        case "scissors-rock":
+            console.log(`Oh no, ${playerSelection} loses to ${computerSelection}! `)
+            break;
+        default:
+            console.log(`Oops, ${playerSelection} isn't a valid choice for this game...`)
+    }
+}
