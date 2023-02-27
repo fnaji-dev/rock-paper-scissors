d4fe91a54990e3e5f59e4965fd59abf57b74b289
Firas Naji <naji.firas@gmail.com>
2023-02-27 16:26:10 +0900

    Write pseudocode to plan out program

---
+++ rps-game.js
@@ -0,0 +1,11 @@
+// Get random computer choice
+
+// Assign computer choice to computer selection
+
+// Ask player for selection input
+
+// Assign player input to player selection
+
+// Check player selection against computer selection
+
+// Inform player of win/loss
