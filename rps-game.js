f4c606f1f0b040e47fdcdd30d9d9c81117316153
Firas Naji <naji.firas@gmail.com>
2023-02-27 16:36:43 +0900

    Create getComputerChoice function
    Get random integer from 0 to 2, return rock/paper/scissors
    Assign return value to computerSelection variable

--- rps-game.js
+++ rps-game.js
@@ -1,7 +1,16 @@
 // Get random computer choice
-
-// Assign computer choice to computer selection
-
+function computerChoice() {
+    let randomInt = Math.floor(Math.random() * 3);
+    if (randomInt === 0) {
+        return "rock";
+    } else if (randomInt === 1) {
+        return "paper";
+    } else {
+        return "scissors";
+    }
+}
+// Assign computer choice to computer selection\
+let computerSelection = computerChoice()
 // Ask player for selection input
 
 // Assign player input to player selection
